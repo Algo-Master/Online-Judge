357215101a89bec8b0c8088ff1171ab850c8149f
Pratham <rimo.coder@gmail.com>
2024-09-13 21:15:32 +0530

    Adding Problems and resolving issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // dotenv.config();
 import "./App.css";
 
-import Landingpage from "./Pages/Landingpage.jsx";
+// import Landingpage from "./Pages/Landingpage.jsx";
 import Loginpage from "./Pages/Loginpage.jsx";
 import Registrationpage from "./Pages/Registrationpage.jsx";
 import Problemlist from "./Pages/Problemlist.jsx";
